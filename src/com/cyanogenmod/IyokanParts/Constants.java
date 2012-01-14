d80fab28dd4f829ea6f31be326309c6bbd0e768f
Pedro Veloso <pedro@pedroT410.(none)>
2012-01-14 18:49:39 +0000

    Added Constants Interface, and ShellInterface class for calling remote commands

---
+++ src/com/cyanogenmod/IyokanParts/Constants.java
@@ -0,0 +1,11 @@
+package com.cyanogenmod.IyokanParts;
+
+/**
+ * User: Pedro Veloso
+ * Date: 1/14/12
+ * Time: 6:41 PM
+ */
+public interface Constants {
+    
+    public static String LOG_TAG = "iyokan_parts";
+}
